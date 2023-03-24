faa08d5dea9126f79fa8954af55e388705465705
Jake Eik <eik995@hotmail.com>
2023-03-23 22:35:23 -0500

    added JS code for project

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+// Character set for potential password options
 var uniChr = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_",
 "-", "=", "+", "`", "~", "[", "]", "{", "}", ";", ":", "'", '"', "|", "<",
 ">", ",", ".", "?", "/", "//", ""];
@@ -11,8 +11,6 @@ var uppChr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
 
 var numChr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0",];
 
-var generateBtn = document.querySelector("#generate");
-
 
 function passwordOptions() {
   var leng = parseFloat(prompt("How long do you want your desired password to be?"), 10);
@@ -24,20 +22,88 @@ function passwordOptions() {
   }
 
   //Checks to see if password length is witin acceptable standards
-  if ((leng < 8) || (leng > 129)) {
+  if ((leng < 8) || (leng > 128)) {
     alert("Password length not within acceptable character limit of 8 - 128");
     return null;
   };
+
+  //Storage variables for various charcter options
+  var hasUnique = confirm("Would you like unique characters in your password?");
+  var hasLower = confirm("Would you like lower case characters in your password?");
+  var hasUpper = confirm("Would you like upper case characters in your password?")
+  var hasNum = confirm("Would you like numerical chaarcters in your password?");
+
+  //check for special characters, reseting if none are found
+  if (hasUnique === false && hasLower === false && hasUpper === false && hasNum === false) {
+    alert("Password must contain at least one special character type")
+    return null;
+  };
+
+  //Creates a password object that stores Boolieans for special characters and length
+  var potenPass = {
+    leng: leng, hasUnique: hasUnique, hasLower: hasLower, hasUpper: hasUpper, hasNum: hasNum
+  };
+
+  return potenPass;
+
 };
+
+//Function to get random element from within an array
+function getRandom(arr) {
+  var randArry = Math.floor(Math.random() * arr.length);
+  var randEle = arr[randArry];
+
+  return randEle;
+}
 
 function generatePassword() {
   var choices = passwordOptions();
 
+  //Variable to store password as it's being built
   var final = [];
-
+  //List of desired characters for our password
   var desChr = [];
   var finChr = [];
+
+  //Checks to see if object exsists, terminates if not
+  if (!choices) 
+  return null;
+
+  //Updates desChr to include desired characters and adds a random character to an auto-include list
+  if (choices.hasUnique) {
+    desChr = desChr.concat(uniChr);
+    finChr.push(getRandom(uniChr));
+  }
+  if (choices.hasLower) {
+    desChr = desChr.concat(lowCrh);
+    finChr.push(getRandom(lowCrh));
+  }
+  if (choices.hasUpper) {
+    desChr = desChr.concat(uppChr);
+    finChr.push(getRandom(uppChr));
+  }
+  if (choices.hasNum) {
+    desChr = desChr.concat(numChr);
+    finChr.push(getRandom(numChr));
+  }
+
+  //Gets a random character from our desired character list and adds it to our final password
+  for (var i = 0; i < choices.leng; i++) {
+    var potenChr = getRandom(desChr);
+
+    final.push(potenChr);
+  };
+
+  //Adds at least one guarenteed random character from out auto-include list 
+  for (var x = 0; x < finChr.length; x++) {
+    final[x] = finChr[x];
+  };
+
+  //
+  return final.join('');
 };
+
+var generateBtn = document.querySelector("#generate");
 
 
 // Write password to the #password input
@@ -46,8 +112,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
